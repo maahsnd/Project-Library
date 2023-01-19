72219755ed507d1bf07464f7ade6269374b914df
nmaahs <105306382+mrwindupbirdesq@users.noreply.github.com>
2023-01-19 16:14:32 +0000

    Add basic style

--- script.js
+++ script.js
@@ -17,75 +17,78 @@ function addBookToLibrary(author, title, pages, readStatus) {
 }
 
 function addBooksToLib() {
-    const tbody = document.getElementsByTagName("tbody")[0];
-    for (let i = bookmark; i < myLibrary.length; i++) {
-        const row = document.createElement("tr");
+  const tbody = document.getElementsByTagName("tbody")[0];
+  for (let i = bookmark; i < myLibrary.length; i++) {
+    const row = document.createElement("tr");
 
-        for (const prop in myLibrary[i]) {
-            let obj = myLibrary[i] ;
-                const cell = document.createElement("td");
-                const cellText = document.createTextNode(`${obj[prop]}`);
-                console.log(cellText);
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-        }
-        //create delete button
-        const delButton = document.createElement("button")
-        delButton.className = 'delButton';
-        delButton.id = `${i}`;
-        delButton.addEventListener("click", deleteBook);
-        row.appendChild(delButton);
-        //create read button
-        const readButton = document.createElement("button")
-        readButton.className = 'readButton';
-        readButton.id = `r${i}`;
-        readButton.addEventListener("click", readBook)
-        row.appendChild(readButton);
-        //name and append row
-        row.className = `row${i}`;
-        tbody.appendChild(row);
-        bookmark += 1;
+    for (const prop in myLibrary[i]) {
+      let obj = myLibrary[i];
+      const cell = document.createElement("td");
+      const cellText = document.createTextNode(`${obj[prop]}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
     }
+    //create delete button
+    const delButton = document.createElement("button");
+    delButton.className = "delButton";
+    delButton.innerHTML = "delete book";
+    delButton.id = `${i}`;
+    delButton.addEventListener("click", deleteBook);
+    row.appendChild(delButton);
+    //create read button
+    const readButton = document.createElement("button");
+    readButton.innerHTML = "read book";
+    readButton.className = "readButton";
+    readButton.id = `r${i}`;
+    readButton.addEventListener("click", readBook);
+    row.appendChild(readButton);
+    //name and append row
+    row.className = `row${i}`;
+    tbody.appendChild(row);
+    bookmark += 1;
+  }
 }
 
 function showNewBookForm() {
-    form[0].style.display = "block";
-    //create form listener
-    const submitBtn = document.querySelector(".submit");
-    submitBtn.addEventListener("click", handleSubmit);
+  form[0].style.display = "block";
+  //create form listener
+  const submitBtn = document.querySelector(".submit");
+  submitBtn.addEventListener("click", handleSubmit);
 }
 
 function handleSubmit(event) {
-    const bookToAdd = document.querySelectorAll("input");
-    myLibrary.push(new Book(
-        bookToAdd[0].value,
-        bookToAdd[1].value,
-        bookToAdd[2].value,
-        bookToAdd[3].value
-    ))
-    form[0].style.display = "none";
-    addBooksToLib();
-    event.preventDefault();
+  const bookToAdd = document.querySelectorAll("input");
+  myLibrary.push(
+    new Book(
+      bookToAdd[0].value,
+      bookToAdd[1].value,
+      bookToAdd[2].value,
+      bookToAdd[3].value
+    )
+  );
+  form[0].style.display = "none";
+  addBooksToLib();
+  event.preventDefault();
 }
 
 function deleteBook(event) {
-    let rowNum = parseInt(event.target.id); 
-    //remove data from library array
-    myLibrary.splice(rowNum);
-    let bookForDeletion = document.querySelector(`.row${rowNum}`);
-    bookForDeletion.remove();
-    bookmark -= 1;
-    addBooksToLib();
+  let rowNum = parseInt(event.target.id);
+  //remove data from library array
+  myLibrary.splice(rowNum);
+  let bookForDeletion = document.querySelector(`.row${rowNum}`);
+  bookForDeletion.remove();
+  bookmark -= 1;
+  addBooksToLib();
 }
 
 function readBook(event) {
-    let rowNum = parseInt((event.target.id).slice(1));
-    console.log(myLibrary[rowNum].readStatus, myLibrary[rowNum])
-    myLibrary[rowNum].readStatus = 'read';
-    console.log(myLibrary[rowNum].readStatus, myLibrary[rowNum])
-    document.querySelector(`.row${rowNum}>td:last-of-type`).innerHTML = 'read';
-    }
+  let rowNum = parseInt(event.target.id.slice(1));
+  myLibrary[rowNum].readStatus = "read";
+  document.querySelector(`.row${rowNum}>td:last-of-type`).innerHTML = "read";
+}
 
+
+//stock the shelves with two classics and a dream
 addBookToLibrary('Patrick Rothfuss', 'The Name of the Wind', '662', 'read');
 addBookToLibrary('Patrick Rothfuss', 'The Wise Man\'s fear', '994', 'read');
 addBookToLibrary('Patrick Rothfuss', 'The Doors of Stone', 'unknown', 'not read');
