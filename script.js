a89141e80adb4e694c8549efe210cfd0237f7c7a
nmaahs <105306382+mrwindupbirdesq@users.noreply.github.com>
2023-01-19 15:27:03 +0000

    Implement read button

--- script.js
+++ script.js
@@ -16,22 +16,32 @@ function addBookToLibrary(author, title, pages, readStatus) {
   myLibrary.push(new Book(author, title, pages, readStatus));
 }
 
-function displayBooks() {
+function addBooksToLib() {
     const tbody = document.getElementsByTagName("tbody")[0];
     for (let i = bookmark; i < myLibrary.length; i++) {
         const row = document.createElement("tr");
 
         for (const prop in myLibrary[i]) {
-            const cell = document.createElement("td");
-            const cellText = document.createTextNode(`${myLibrary[i][prop]}`);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+            let obj = myLibrary[i] ;
+                const cell = document.createElement("td");
+                const cellText = document.createTextNode(`${obj[prop]}`);
+                console.log(cellText);
+                cell.appendChild(cellText);
+                row.appendChild(cell);
         }
+        //create delete button
         const delButton = document.createElement("button")
         delButton.className = 'delButton';
         delButton.id = `${i}`;
         delButton.addEventListener("click", deleteBook);
         row.appendChild(delButton);
+        //create read button
+        const readButton = document.createElement("button")
+        readButton.className = 'readButton';
+        readButton.id = `r${i}`;
+        readButton.addEventListener("click", readBook)
+        row.appendChild(readButton);
+        //name and append row
         row.className = `row${i}`;
         tbody.appendChild(row);
         bookmark += 1;
@@ -54,22 +64,29 @@ function handleSubmit(event) {
         bookToAdd[3].value
     ))
     form[0].style.display = "none";
-    displayBooks();
+    addBooksToLib();
     event.preventDefault();
 }
 
 function deleteBook(event) {
-    console.log(event.target.id);
     let rowNum = parseInt(event.target.id); 
     //remove data from library array
     myLibrary.splice(rowNum);
     let bookForDeletion = document.querySelector(`.row${rowNum}`);
     bookForDeletion.remove();
     bookmark -= 1;
-    displayBooks();
+    addBooksToLib();
 }
+
+function readBook(event) {
+    let rowNum = parseInt((event.target.id).slice(1));
+    console.log(myLibrary[rowNum].readStatus, myLibrary[rowNum])
+    myLibrary[rowNum].readStatus = 'read';
+    console.log(myLibrary[rowNum].readStatus, myLibrary[rowNum])
+    document.querySelector(`.row${rowNum}>td:last-of-type`).innerHTML = 'read';
+    }
 
 addBookToLibrary('Patrick Rothfuss', 'The Name of the Wind', '662', 'read');
 addBookToLibrary('Patrick Rothfuss', 'The Wise Man\'s fear', '994', 'read');
 addBookToLibrary('Patrick Rothfuss', 'The Doors of Stone', 'unknown', 'not read');
-displayBooks();
+addBooksToLib();
